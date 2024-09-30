95177f2e9af0321cb2a2c9b9fede336cb1a0550c
jiwon.ha <jiwon.ha@terra-korea.com>
2024-09-30 14:01:11 +0900

    메인 페이지 개발

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,126 @@
+"use client";
+
+import Tooltip from "@/components/Tooltip/Tooltip";
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import styled, { useTheme } from "styled-components";
 
 export default function Home() {
+  const theme = useTheme();
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Container>
+      <First>
+        <Image src="/image/me.png" alt="me" width={160} height={160} />
+        <RightSection>
+          <span className="name">JIWON</span>
+          <span className="intro">어쩌고 저쩌고 소개입니다.</span>
+          <IconWrap>
+            <Git>
+              <Link href="https://github.com/magok-developer" target="_blank">
+                <Image
+                  src={theme.icons.github}
+                  alt="git"
+                  width={22}
+                  height={22}
+                />
+              </Link>
+              <Tooltip
+                text="GitHub"
+                className="git"
+                style={{ left: "-10px" }}
+              />
+            </Git>
+            <Resume>
+              <Link
+                href="https://giddy-product-b00.notion.site/1248f4226cca4b8aaf0d694951985aad"
+                target="_blank"
+              >
+                <Image
+                  src={theme.icons.resume}
+                  alt="resume"
+                  width={22}
+                  height={22}
+                />
+              </Link>
+              <Tooltip
+                text="resume"
+                className="resume"
+                style={{ left: "-13px" }}
+              />
+            </Resume>
+          </IconWrap>
+        </RightSection>
+      </First>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <Archives>
+        <div className="title">Archives</div>
+      </Archives>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const RightSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 0;
+  gap: 12px;
+
+  .name {
+    font-size: 30px;
+    font-weight: bold;
+  }
+
+  .intro {
+    font-size: 14px;
+  }
+`;
+
+const IconWrap = styled.div`
+  margin-top: 30px;
+  display: flex;
+  gap: 12px;
+
+  .git {
+    opacity: 0;
+  }
+
+  .resume {
+    opacity: 0;
+  }
+`;
+
+const Git = styled.div`
+  position: relative;
+  &:hover {
+    .git {
+      opacity: 1;
+    }
+  }
+`;
+
+const Resume = styled.div`
+  position: relative;
+  &:hover {
+    .resume {
+      opacity: 1;
+    }
+  }
+`;
+
+const First = styled.div`
+  display: flex;
+`;
+
+const Archives = styled.div`
+  padding: 100px 40px;
+  .title {
+    font-size: 40px;
+    font-weight: bold;
+  }
+`;
