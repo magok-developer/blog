9f4ef305c8550ac04901f3cba4b5a8803afede02
jiwon.ha <jiwon.ha@terra-korea.com>
2024-09-30 13:59:46 +0900

    RootContainer 추가

---
+++ src/components/RootContainer.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import styled, { ThemeProvider } from "styled-components";
+import GlobalStyle from "@/styles/GlobalStyle";
+import { darkTheme, lightTheme } from "@/styles/theme";
+import Header from "./Header/Hedaer";
+
+const RootContainer = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "light" ? lightTheme : darkTheme;
+  });
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) =>
+      prevTheme === lightTheme ? darkTheme : lightTheme
+    );
+  };
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme === darkTheme ? "dark" : "light");
+  }, [theme]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle theme={theme} />
+      <Header onClick={toggleTheme} isChecked={theme === lightTheme} />
+      <Body>{children}</Body>
+    </ThemeProvider>
+  );
+};
+
+export default RootContainer;
+
+const Body = styled.div`
+  padding: 100px 200px;
+`;
