c833cb77ed19022dfd6a2b6fc7d6ec4f57382a06
jiwon.ha <jiwon.ha@terra-korea.com>
2024-09-30 14:00:41 +0900

    Theme toggle 컴포넌트 개발

---
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,36 @@
+// import { mainColor } from "@/styles/theme";
+import Image from "next/image";
+// import styled from "styled-components";
+
+type Props = {
+  onClick: () => void;
+  isChecked: boolean;
+};
+
+const ThemeToggle = ({ onClick, isChecked }: Props) => {
+  return (
+    <>
+      {isChecked ? (
+        <Image
+          src="/icons/dark.svg"
+          alt="dark"
+          width={20}
+          height={20}
+          onClick={onClick}
+          style={{ cursor: "pointer" }}
+        />
+      ) : (
+        <Image
+          src="/icons/light.svg"
+          alt="dark"
+          width={20}
+          height={20}
+          onClick={onClick}
+          style={{ cursor: "pointer" }}
+        />
+      )}
+    </>
+  );
+};
+
+export default ThemeToggle;
